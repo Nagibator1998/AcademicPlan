c5f0e919a919701cea26a885ee578e04d8858596
Nagibator1998 <zholudev.evgeniy@mail.ru>
2020-01-04 13:15:52 +0300

    commit 4

--- AcademicPlanFrontend/src/app/app.module.ts
+++ AcademicPlanFrontend/src/app/app.module.ts
@@ -4,15 +4,29 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './component/header/header.component';
 import { FooterComponent } from './component/footer/footer.component';
+import {RouterModule, Routes} from '@angular/router';
+import { CreatePlanComponent } from './component/create-plan/create-plan.component';
+import { JumbotronComponent } from './component/jumbotron/jumbotron.component';
+
+const routes: Routes = [
+  {path: '', redirectTo: 'jumbotron', pathMatch: 'full'},
+  {path: 'create-plan', component: CreatePlanComponent},
+  {path: 'jumbotron', component: JumbotronComponent},
+  {path: '*', redirectTo: '/'}
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    CreatePlanComponent,
+    JumbotronComponent
   ],
+  exports: [RouterModule],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
